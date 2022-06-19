bdbec20455e82b28c6617dfcf5ebb332e5f9b5a1
chandrakalasv <chandrakalasv1502@gmail.com>
2022-06-19 12:14:30 +0530

    added message on master branch as binary search tree implementation

---
+++ src/com/bridgelab/binary/BinaryTreeMain.java
@@ -0,0 +1,7 @@
+package com.bridgelab.binary;
+
+public class BinaryTreeMain {
+    public static void main(String[] args) {
+        System.out.println("binary search tree implementation");
+    }
+}
